2946f9b51a826e6c4253273b9fe10842dfd65aa0
alexhkz <alexwebcode@gmail.com>
2020-07-08 21:27:17 +0300

    add classes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-	
+
 	//Tabs
 
 	const tabs = document.querySelectorAll('.tabheader__item'),
-			tabsContent = document.querySelectorAll('.tabcontent'),
-			tabsParent = document.querySelector('.tabheader__items');
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
 
 	function hideTabContent() {
 		tabsContent.forEach(item => {
@@ -45,10 +45,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function getTimeRemaining(endtime) {
 		const t = Date.parse(endtime) - Date.parse(new Date()),
-				days = Math.floor(t / (1000 * 60 * 60 * 24)),
-				hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-				minutes = Math.floor((t / 1000 / 60) % 60),
-				seconds = Math.floor((t / 1000) % 60);
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
 
 		return {
 			'total': t,
@@ -69,12 +69,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function setClock(selector, endtime) {
 		const timer = document.querySelector(selector),
-				days = timer.querySelector('#days'),
-				hours = timer.querySelector('#hours'),
-				minutes = timer.querySelector('#minutes'),
-				seconds = timer.querySelector('#seconds'),
-				timeInterval = setInterval(updateClock, 1000);
-		
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
 		updateClock();
 
 		function updateClock() {
@@ -96,8 +96,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	//Modal
 
 	const modalTrigger = document.querySelectorAll('[data-modal]'),
-			modal = document.querySelector('.modal'),
-			modalCloseBtn = document.querySelector('[data-close]');
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]');
 
 	function openModal() {
 		modal.classList.add('show');
@@ -130,7 +130,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	const modalTimerId = setTimeout(openModal, 5000);
+	// const modalTimerId = setTimeout(openModal, 5000);
 
 	function showModalByScroll() {
 		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -140,4 +140,81 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	window.addEventListener('scroll', showModalByScroll);
+
+	// Используем классы для карточек
+
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 27;
+			this.changeToUA();
+		}
+
+		changeToUA() {
+			this.price = this.price * this.transfer;
+		}
+
+		render() {
+			const element = document.createElement('div');
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
+
+			element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+	// const div = new MenuCard();
+	// div.render();
+	// альтернативный вариант записи, когда нужно только единажды вызвать.
+
+	new MenuCard(
+		"img/tabs/vegy.jpg",
+		"vegy",
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		9,
+		'.menu .container',
+		'menu__item',
+		'big'
+	).render();
+
+	new MenuCard(
+		"img/tabs/elite.jpg",
+		"elite",
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		14,
+		'.menu .container',
+		'menu__item'
+	).render();
+
+	new MenuCard(
+		"img/tabs/post.jpg",
+		"post",
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		21,
+		'.menu .container',
+		'menu__item'
+	).render();
 });
